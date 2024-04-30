5ee1035544cfbb207c86405f85cdac3f8636ba27
syso123 <malno@post.bgu.ac.il>
2024-04-30 21:05:57 +0300

    initial

---
+++ pages/searchResults/static/js/searchResults.js
@@ -0,0 +1,16 @@
+function showPhone(phoneNumber) {
+    const phoneNumberDisplay = document.getElementById("phoneNumberDisplay");
+    phoneNumberDisplay.innerHTML = "Phone number: " + phoneNumber;
+   // business = session()
+   // update_click_count()
+}
+
+function changeBusiness(future_index, total_count, cat) {
+    if (future_index >= total_count)
+        future_index = 0
+    else if (future_index < 0){
+        future_index = total_count - 1
+    }
+
+    window.location.href = `${window.location.pathname}?cat=${cat}&index=${future_index}`;
+    }
